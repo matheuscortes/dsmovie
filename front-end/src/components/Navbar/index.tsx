e6f3462a1664dd65ce839dff9f04912c8ee80fd5
MatheusCortes <mcortes1245@gmail.com>
2022-05-15 00:21:59 -0300

    Saved score and used useNavigate

--- front-end/src/components/Navbar/index.tsx
+++ front-end/src/components/Navbar/index.tsx
@@ -1,12 +1,16 @@
 import './style.css'; 
 import { ReactComponent as GitHubIcon } from 'assets/github.svg'; 
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
     return (
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <h1 onClick={() => navigate("/")}>DSMovie</h1>
                     <a href="https://github.com/matheuscortes">
                         <div className='dsmovie-contact-container'>
                             <GitHubIcon />
